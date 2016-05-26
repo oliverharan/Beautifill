9b623eb6147fe8e32930b54656e515e0a3510b50
oliverharan <harano@live.com>
2016-05-26 18:32:15 -0400

    tesst

--- Beautifill/Beautifill/Content/scripts.js
+++ Beautifill/Beautifill/Content/scripts.js
@@ -18,7 +18,7 @@
 
 // });
 
-(function () {
+(function() {
 
 
     //Scrollmagic2
@@ -66,12 +66,12 @@
     //    .addTo(controller);
 
 
-    //var controller = $.superscrollorama();
-    //// individual element tween examples
-    //controller.addTween('#title1', TweenMax.from( $('#title1'), .5, {css:{opacity: 0}}));
-    ////controller.addTween('#title2', TweenMax.from( $('#title2'), .25, {css:{right:'1000px'}, ease:Quad.easeInOut}));
-    //controller.addTween('#title3', TweenMax.from( $('#title3'), .25, {css:{opacity:0, rotation: 720}, ease:Quad.easeOut}));
-    //controller.addTween('#title2', TweenMax.fromTo( $('#title2'), .25, {css:{opacity:0, fontSize:'20px'}, immediateRender:true, ease:Quad.easeInOut}, {css:{opacity:1, fontSize:'240px'}, ease:Quad.easeInOut}));
-    ////controller.addTween('#smush-it', TweenMax.fromTo( $('#smush-it'), .25, {css:{opacity:0, 'letter-spacing':'30px'}, immediateRender:true, ease:Quad.easeInOut}, {css:{opacity:1, 'letter-spacing':'-10px'}, ease:Quad.easeInOut}), 0, 100); // 100 px offset for better timing
+			//var controller = $.superscrollorama();
+			//// individual element tween examples
+			//controller.addTween('#title1', TweenMax.from( $('#title1'), .5, {css:{opacity: 0}}));
+			////controller.addTween('#title2', TweenMax.from( $('#title2'), .25, {css:{right:'1000px'}, ease:Quad.easeInOut}));
+			//controller.addTween('#title3', TweenMax.from( $('#title3'), .25, {css:{opacity:0, rotation: 720}, ease:Quad.easeOut}));
+			//controller.addTween('#title2', TweenMax.fromTo( $('#title2'), .25, {css:{opacity:0, fontSize:'20px'}, immediateRender:true, ease:Quad.easeInOut}, {css:{opacity:1, fontSize:'240px'}, ease:Quad.easeInOut}));
+			////controller.addTween('#smush-it', TweenMax.fromTo( $('#smush-it'), .25, {css:{opacity:0, 'letter-spacing':'30px'}, immediateRender:true, ease:Quad.easeInOut}, {css:{opacity:1, 'letter-spacing':'-10px'}, ease:Quad.easeInOut}), 0, 100); // 100 px offset for better timing
 
 })(jQuery);
